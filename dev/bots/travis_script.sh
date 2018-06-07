8c40bf4c5c9541637e39d6cea0e861543e0f45c5
JZoom <>
2018-06-07 13:55:15 +0800

    1.0.0

--- dev/bots/travis_script.sh
+++ dev/bots/travis_script.sh
@@ -8,12 +8,12 @@ if [[ "$SHARD" == "dartfmt" ]]; then
   dartfmt --dry-run --set-exit-if-changed packages || exit $?
 elif [[ "$SHARD" == "analyze" ]]; then
   for package in "${PACKAGES[@]}"; do
-    echo "Analyzing packages/$package"
-    cd $ROOT/packages/$package
+    echo "Analyzing package "
+    cd $ROOT/packages
     dartanalyzer --options=$ROOT/analysis_options.yaml . || exit $?
   done
 else
   # tests shard
   cd $ROOT/package
-  pub run test -j1 -rexpanded || exit $?
+  flutter --no-color test --machine --start-paused test/infinity_page_view_test.dart || exit $?
 fi
